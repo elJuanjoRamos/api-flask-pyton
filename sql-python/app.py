4400881af15415995d468a3fdf3878b18af5ce5a
eljuanjoramos <eljuanjo.ramos@gmail.com>
2021-10-05 17:39:22 -0600

    updated

--- sql-python/app.py
+++ sql-python/app.py
@@ -1,11 +1,13 @@
 import mysql.connector as mysql
-import azure.cosmos.cosmos_client as cosmos_client
 import os
 import pymongo
-
+import json
+from datetime import datetime
 from operator import itemgetter
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv, find_dotenv
+from google.cloud import pubsub_v1
+from google.auth import jwt
 load_dotenv(find_dotenv())
 
 # ENV VARIABLES SQL
@@ -24,6 +26,8 @@ COSMOS_CONTAINER = os.environ.get("COSMOS_CONTAINER")
 
 CLOUD_MONGO_DB = os.environ.get('CLOUD_MONGO_DB')
 
+print(CLOUD_MONGO_DB)
+
 # FLASK CONFIG
 app = Flask(__name__)
 app.debug = True
@@ -40,6 +44,16 @@ myMongoClient = None
 myMongoDb = None
 myMongoCollection = None
 
+project_id = "sopes-326122"
+topic_id = "notificacion-carga"
+#publisher = pubsub_v1.PublisherClient()
+#topic_path = publisher.topic_path(project_id, topic_id)
+
+cantidad_sql = 0
+cantidad_mongo = 0
+tiempo = datetime.now()
+
+
 def parse_date(date):
     current = date.split('/')
     # YY -- MM -- DD
@@ -50,7 +64,8 @@ def parse_date(date):
 def home():
     return jsonify({"message": "api montada correctamente"})
 
-@app.route('/iniciaCarga', methods=['GET'])
+
+@app.route('/iniciarCarga', methods=['GET'])
 def init():
     global cloud
     global client
@@ -61,19 +76,17 @@ def init():
     global myMongoDb
     global myMongoCollection
     try:
+        print(CLOUD_SQL_HOST)
+        print(CLOUD_SQL_USER)
+        print(CLOUD_SQL_PASS)
         # CONEXION MYSQL
         db = mysql.connect(host=CLOUD_SQL_HOST, user=CLOUD_SQL_USER, password=CLOUD_SQL_PASS, database=CLOUD_SQL_DATA)
         cursor = db.cursor()
-        # CONEXION AZURE SQL
-        #client = cosmos_client.CosmosClient(ACCOUNT_HOST, {'masterKey': ACCOUNT_KEY},
-        #                                    user_agent="CosmosDBPythonQuickstart", user_agent_overwrite=True)
-        #cosmo_db = client.get_database_client(COSMOS_DATABASE)
-        #container = cosmo_db.get_container_client(COSMOS_CONTAINER)
         # CONEXION AZURE MONGO DB
         myMongoClient = pymongo.MongoClient(CLOUD_MONGO_DB)
+        print(myMongoClient)
         myMongoDb = myMongoClient['Tweets']
         myMongoCollection = myMongoDb['Tweet']
-
         return jsonify({"message": "Conexion creada con exito"})
     except Exception as e:
         print(e)
@@ -82,8 +95,11 @@ def init():
 
 @app.route('/publicar', methods=['POST'])
 def send():
+    global cantidad_sql
+    global cantidad_mongo
+    status = 200
+    dbInsertar = ""
 
-    status = 400
     tweetData = request.get_json()
     nombre, comentario, fecha, upvotes,  downvotes, \
     hashtags = itemgetter('nombre', 'comentario', 'fecha' , 'upvotes', 'downvotes', 'hashtags')(tweetData)
@@ -99,38 +115,60 @@ def send():
         query = 'INSERT INTO Tweet(nombre, comentario, fecha, upvotes, downvotes, hashtags) VALUES(%s,%s,%s,%s,%s,%s)'
         cursor.execute(query, (nombre, comentario, parse_date(fecha), upvotes, downvotes, list))
         db.commit()
-
-        tweetData['id'] = str(cursor.lastrowid)
-        tweetData['pk'] = str(cursor.lastrowid)
-
-        status = 200
+        cantidad_sql += 1
     except Exception as e:
+        status = 400
+        dbInsertar = "SQL"
         print(e)
-        return jsonify({"message": "error to insert en db google"}), 400
-    #if status == 200:
-    #    try:
-    #        print('\nCreating Items into AZURE SQL\n')
-    #        container.create_item(body=tweetData)
-    #        status = 200
-    #    except Exception as e:
-    #        return jsonify({"message": "error to insert en cosmo db"}), 400
-    if status == 200:
-        try:
-            print('\nCreating Items into AZURE MONGO\n')
-            myMongoCollection.insert_one(tweetData)
-            return jsonify({"message": "tweet saved"})
-        except Exception as e:
-            return jsonify({"message": "error to insert en cosmo db"}), 400
+    try:
 
+        myMongoCollection.insert_one(tweetData)
+        cantidad_mongo += 1
+    except Exception as e:
+        status = 400
+        dbInsertar = "Cosmo"
+        print(e)
 
+    if status == 400:
+        return jsonify({"message": f'error to insert en { dbInsertar } db'}), 400
 
+    return jsonify({"message": 'Tweet guardado con exito'}), 200
 
 
 @app.route('/finalizarCarga', methods=['GET'])
 def close():
+    global tiempo
+    time = datetime.strptime(parse_tiempo(datetime.now()),'%H:%M:%S')-datetime.strptime(parse_tiempo(tiempo),'%H:%M:%S')
     global db
-    db.disconnect()
+    try:
+        db.disconnect()
+    except Exception as e:
+        pass
+
+    service_account_info = json.load(open("sopes-326122-3cdb97cea07b.json"))
+    audience = "https://pubsub.googleapis.com/google.pubsub.v1.Subscriber"
+    credentials = jwt.Credentials.from_service_account_info(
+        service_account_info, audience=audience
+    )
+    publisher_audience = "https://pubsub.googleapis.com/google.pubsub.v1.Publisher"
+    credentials_pub = credentials.with_claims(audience=publisher_audience)    
+    publisher = pubsub_v1.PublisherClient(credentials=credentials_pub)
+    topic_path = publisher.topic_path(project_id, topic_id)
+    data = {
+        "api":"python",
+        "cantidad": cantidad_sql + cantidad_mongo,
+        "tiempoDeCarga": str(time)
+    }
+    datas = json.dumps(data)
+    datas = datas.encode("utf-8")
+    future = publisher.publish(
+        topic_path, datas, origin="api-python", username = "data"
+    )
     return jsonify({"message": "connection closed"})
+
+
+def parse_tiempo(time):
+    return str(time.hour) + ":" + str(time.minute) + ":" + str(time.second)
 
 
 if __name__ == '__main__':
